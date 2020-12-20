dc7b120953415c1e152372cd90d7617aadc83fc7
Mittul Mandhan <conanmittul@gmail.com>
2020-12-20 07:22:29 +0530

    initial commit

---
+++ src/main/java/com/hacksterfest/hacksterfest/HacksterFestApplication.java
@@ -0,0 +1,13 @@
+package com.hacksterfest.hacksterfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacksterFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacksterFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacksterfest/hacksterfest/HacksterFestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacksterfest.hacksterfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacksterFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
